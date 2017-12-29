2e417f533ecf0e437abcb04fafd65205a8e54c04
slovi <slovik421@gmail.com>
2017-12-29 22:30:25 +0100

    added Entities

---
+++ src/AppBundle/Entity/Lekarz.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: slovi
+ * Date: 29.12.2017
+ * Time: 22:18
+ */
+
+namespace AppBundle\Entity;
+
+
+class Lekarz
+{
+
+}
---
+++ src/AppBundle/Entity/Pacjent.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: slovi
+ * Date: 29.12.2017
+ * Time: 22:17
+ */
+
+namespace AppBundle\Entity;
+
+
+class Pacjent
+{
+
+}
